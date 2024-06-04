9d7b98276b345b8ae86df195812ff6dc5db01225
laura2ndrea <laura2ndrea12@gmail.com>
2024-06-04 16:45:03 -0500

    feat: creacion de estructuras del menu

---
+++ menu/menus.py
@@ -0,0 +1,153 @@
+import menu.opciones as menuOpciones
+
+
+def menu_principal():
+     while True: 
+        menuOpciones.separador()
+        print("Bienvenido al menú principal, por favor indique su rol: ")
+        opcion = menuOpciones.recorrer_opciones(menuOpciones.opc_principal)
+        if opcion == "1": 
+           menu_coordinador()
+        elif opcion == "2": 
+            menu_trainer()
+        elif opcion == "3": 
+            menu_camper()
+        elif opcion == "4": 
+            print("Saliendo ...")
+            break
+        else: 
+            print ("Valor invalido, por favor ingrese una opcion nuevamente")
+
+def menu_coordinador(): 
+    while True: 
+        menuOpciones.separador()
+        print("Bienvenido coordinador, por favor seleccione una opción: ")
+        opcion = menuOpciones.recorrer_opciones(menuOpciones.opc_coordinacion)
+        if opcion == "1": 
+           menu_gestionMatricula()
+        elif opcion == "2": 
+            menu_gestionReportes()
+        elif opcion == "3": 
+            menu_gestionRutas()
+        elif opcion == "4": 
+            menu_gestionCampers()
+        elif opcion == "5": 
+            print("Saliendo ...")
+            break
+        else: 
+            print ("Valor invalido, por favor ingrese una opcion nuevamente")
+
+def menu_trainer(): 
+    while True: 
+        menuOpciones.separador()
+        print("Bienvenido trainer, por favor seleccione una opción: ")
+        opcion = menuOpciones.recorrer_opciones(menuOpciones.opc_trainers)
+        if opcion == "1": 
+           print("En construccion")
+        elif opcion == "2": 
+            print("trainers")
+        elif opcion == "3": 
+            print ("campers")
+        elif opcion == "4": 
+            print("Saliendo ...")
+            break
+        else: 
+            print ("Valor invalido, por favor ingrese una opcion nuevamente")
+
+def menu_camper(): 
+    while True: 
+        menuOpciones.separador()
+        print("Bienvenido camper, por favor seleccione una opción: ")
+        opcion = menuOpciones.recorrer_opciones(menuOpciones.opc_campers)
+        if opcion == "1": 
+           print("En construccion")
+        elif opcion == "2": 
+            print("trainers")
+        elif opcion == "3": 
+            print ("campers")
+        elif opcion == "4": 
+            print ("En construcción ...")
+        elif opcion == "5": 
+            print("Saliendo ...")
+            break
+        else: 
+            print ("Valor invalido, por favor ingrese una opcion nuevamente")
+
+def menu_gestionMatricula(): 
+    while True: 
+        menuOpciones.separador()
+        print("Por favor seleccione una opción: ")
+        opcion = menuOpciones.recorrer_opciones(menuOpciones.opc_gestionMatricula)
+        if opcion == "1": 
+           print("En construccion")
+        elif opcion == "2": 
+            print("trainers")
+        elif opcion == "3": 
+            print ("campers")
+        elif opcion == "4": 
+            print ("En construcción ...")
+        elif opcion == "5": 
+            print("Saliendo ...")
+            break
+        else: 
+            print ("Valor invalido, por favor ingrese una opcion nuevamente")
+
+def menu_gestionReportes(): 
+    while True: 
+        menuOpciones.separador()
+        print("Por favor seleccione una opción: ")
+        opcion = menuOpciones.recorrer_opciones(menuOpciones.opc_gestionReportes)
+        if opcion == "1": 
+           print("En construccion")
+        elif opcion == "2": 
+            print("trainers")
+        elif opcion == "3": 
+            print ("campers")
+        elif opcion == "4": 
+            print ("En construcción ...")
+        elif opcion == "5": 
+            print ("En construcción ...")
+        elif opcion == "6": 
+            print ("En construcción ...")
+        elif opcion == "7": 
+            print("Saliendo ...")
+            break
+        else: 
+            print ("Valor invalido, por favor ingrese una opcion nuevamente")
+
+def menu_gestionRutas(): 
+    while True: 
+        menuOpciones.separador()
+        print("Por favor seleccione una opción: ")
+        opcion = menuOpciones.recorrer_opciones(menuOpciones.opc_gestionRutas)
+        if opcion == "1": 
+           print("En construccion")
+        elif opcion == "2": 
+            print("trainers")
+        elif opcion == "3": 
+            print ("campers")
+        elif opcion == "4": 
+            print("Saliendo ...")
+            break
+        else: 
+            print ("Valor invalido, por favor ingrese una opcion nuevamente")
+
+def menu_gestionCampers(): 
+    while True: 
+        menuOpciones.separador()
+        print("Por favor seleccione una opción: ")
+        opcion = menuOpciones.recorrer_opciones(menuOpciones.opc_gestionCampers)
+        if opcion == "1": 
+           print("En construccion")
+        elif opcion == "2": 
+            print("trainers")
+        elif opcion == "3": 
+            print ("campers")
+        elif opcion == "4": 
+            print ("En construcción ...")
+        elif opcion == "5": 
+            print("Saliendo ...")
+            break
+        else: 
+            print ("Valor invalido, por favor ingrese una opcion nuevamente")
+
---
+++ menu/opciones.py
@@ -0,0 +1,28 @@
+opc_principal = ("1. Coordinación", "2. Trainers", "3. Campers", "4. Salir del programa")
+
+opc_coordinacion = ("1. Gestión de matrículas", "2. Gestión de reportes", "3. Gestión de rutas", "4. Gestión de campers", "5. Salir del menú")
+
+opc_gestionMatricula = ("1. Asignación de campers a rutas", "2. Asignacion de trainers a rutas", "3. Asignacion de fechas a rutas", "4. Asignación de salones a rutas", "5. Salir del menú")
+
+opc_gestionReportes = ("1. Mostrar campers inscritos", "2. Mostrar campers aprobados", "3. Mostrar lista de trainers", "4. Mostrar campers de bajo rendimiento", "5. Mostrar trainers y campers asociados a rutas de entrenamiento", "6. Mostrar cantidad de campers aprobados y no aprobados por módulo", "7. Salir del menú")
+
+opc_gestionRutas = ("1. Mostrar rutas de entrenamiento", "2. Crear nueva ruta de entrenamiento", "3. Crear nuevo grupo", "4. Salir del menú")
+
+opc_gestionCampers = ("1. Inscripción de nuevo camper", "2. Actualizar información de campers", "3. Ingresar notas de prueba de ingreso", "4. Crear advertencias para campers de alto riesgo", "5. Salir del menú")
+
+opc_trainers = ("1. Mostrar información personal", "2. Mostrar rutas asignadas", "3. Ingresar notas", "4. Salir del menú")
+
+opc_campers = ("1. Mostrar información personal", "2. Mostrar información de ruta asignada", "3. Mostrar notas", "4. Mostrar advertencias", "5. Salir del menú")
+
+def recorrer_opciones(opciones): 
+    for i in opciones:
+        print(i)
+    separador()
+    opcion = input("Ingrese la opción deseada: ")
+    return opcion
+
+def separador(): 
+    print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+
+def mini_separador(): 
+    print("**************************")
